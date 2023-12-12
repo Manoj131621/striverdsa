202ef76bd8f8d84ddfdc64a0a3395547fd606ad4
Manojkumar <manojprfp@gmail.com>
2023-12-12 20:09:24 +0530

    hello program

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class hello{
+    public static void main(String args[]){
+        int a=1,b=2;
+        int c=a+b;
+        System.out.println("c:"+c);
+        System.out.println("Hello world");
+    }
+}
